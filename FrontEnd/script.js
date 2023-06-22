eb01d9257859cea0b6c2f2891a1917623b811da0
valentinmth99 <126247282+valentinmth99@users.noreply.github.com>
2023-06-22 02:22:56 +0200

    Clean script.js

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -37,38 +37,3 @@ exeAPI.then(() => {
         caption.innerText = tableWorks[i].title;
     }   
 })
-// FirstRequestToGraph(AccessToken).then(function() {
-//     alert('testing1234');
-// });
-
-// function RequestNextPage(NextPage) {
-//     return fetch(NextPage, {
-//         method: 'GET'
-//     })
-//     .then(function(response) {
-//         return response.json();
-//     })
-//     .then(function(json) {
-//         RequestNextPage(json.paging.next);
-//     })
-//     .catch(function(err) {
-//         console.log(`Error: ${err}`)
-//     });
-// }
-
-// function FirstRequestToGraph(AccessToken) {
-//     return fetch('https://graph.facebook.com/v2.8/me?fields=posts.limit(275){privacy}%2Cname&access_token=' + AccessToken, {
-//         method: 'GET'
-//     })
-//     .then(function(response) {
-//         return response.json();
-//     })
-//     .then(function(json) {
-//         if(json.data.length !== 0 ){
-//            return RequestNextPage(json.paging.next);
-//         }
-//     })
-//     .catch(function(err) {
-//         console.log(`Error: ${err}`)
-//     });
-// }
